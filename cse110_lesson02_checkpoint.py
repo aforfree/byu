d05d18c96fcf93eba8fde0537d839527743dc211
cctv0 <aforfree@gmail.com>
2023-01-05 14:48:40 +0500

    assignment completed

---
+++ cse110_lesson02_checkpoint.py
@@ -0,0 +1,12 @@
+# CSE110 lesson 02
+# by Evgeny Kozlov
+# github.com/aforfree/byu
+
+
+# User input
+first_name =  input("What is your first name? ").capitalize()
+last_name =  input("What is your last name? ").capitalize()
+
+# Answer
+print(f"\nYour name is {last_name}, {first_name} {last_name}.")
+
