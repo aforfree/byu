d03a17cc151f3bad854f871aa705f27974989600
cctv0 <aforfree@gmail.com>
2023-01-05 12:03:36 +0500

    CSE110 lesson 01

---
+++ cse110_lesson01.py
@@ -0,0 +1,10 @@
+
+# User input
+input_name = input("Hello there, what's your name (press enter after): ")
+input_color = input("and what's your favorite color (press enter after): ")
+
+print("Oh, I guess your favorite color is:")
+# Print the color
+print(input_color)
+print("That's one of my favorite colors too!")
+print(f"Bye, {input_name}!")
