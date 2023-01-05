acc3617669989bd25ef19c9f98d8216b79596173
cctv0 <aforfree@gmail.com>
2023-01-05 12:54:12 +0500

    refactoring, spaces

--- cse110_lesson01.py
+++ cse110_lesson01.py
@@ -1,10 +1,19 @@
+# CSE110 lesson 01
+# by Evgeny Kozlov
+# github.com/aforfree/byu
+
+# colored 'color' word
+OUTPUT_COLOR_WORD = f"\033[91m{'c'}\033[00m\033[92m{'o'}\033[00m\033[93m{'l'}\033[00m\033[94m{'o'}\033[00m\033[95m{'r'}\033[00m"
 
 # User input
-input_name = input("Hello there, what's your name (press enter after): ")
-input_color = input("and what's your favorite color (press enter after): ")
+input_name = input(f"Hello there, what's your name (press enter after): ")
+input_color = input(
+    f"and what's your favorite {OUTPUT_COLOR_WORD} (press enter after): ")
 
-print("Oh, I guess your favorite color is:")
-# Print the color
-print(input_color)
-print("That's one of my favorite colors too!")
-print(f"Bye, {input_name}!")
+# Answer
+print(f"\nOh, I guess your favorite {OUTPUT_COLOR_WORD} is:")
+# Print the color in bold
+print("\033[01m", input_color, "\033[00m")
+print(f"That's one of my favorite {OUTPUT_COLOR_WORD}s too!")
+# Farewell on a grey background
+print("\033[47m", f"Bye, {input_name}!", "\033[0m")
